366368dc75a475317b289fef7f310dad856d722b
Faima Saad Naik <fatima.saadn02@gmail.com>
2024-07-04 15:09:11 +0500

    Update client/docs

---
+++ client/docs/client/server/chunk-FQDPZFCM.mjs
@@ -0,0 +1,38 @@
+import './polyfills.server.mjs';
+import{Ea as e,Fa as n,Ia as t,Ka as s,U as r}from"./chunk-QGOFUSSP.mjs";import"./chunk-NDYDZJSS.mjs";var c=(()=>{let i=class i{};i.\u0275fac=function(a){return new(a||i)},i.\u0275cmp=r({type:i,selectors:[["app-home"]],standalone:!0,features:[s],decls:16,vars:0,consts:[[1,"container"]],template:function(a,l){a&1&&(e(0,"head")(1,"title"),t(2,"Home Page"),n()(),e(3,"body")(4,"header")(5,"h1"),t(6,"Welcome to Our Website"),n()(),e(7,"div",0)(8,"h2"),t(9,"Lorem Ipsum Dolor"),n(),e(10,"p"),t(11,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."),n(),e(12,"h2"),t(13,"Proin Gravida Nibh Vel"),n(),e(14,"p"),t(15,"Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris."),n()()())},styles:[`body[_ngcontent-%COMP%] {
+      font-family: sans-serif;
+      margin: 0;
+      background: linear-gradient(to bottom right, #EFDBEE, #E3BEE1); 
+
+      color: #65365C; 
+
+    }
+
+    header[_ngcontent-%COMP%] {
+      background-color: #FAF5FA; 
+
+      padding: 1rem;
+      text-align: center;
+    }
+
+    h1[_ngcontent-%COMP%] {
+      margin-bottom: 0;
+    }
+
+    .container[_ngcontent-%COMP%] {
+      max-width: 40rem;
+      margin: 2rem auto;
+      padding: 1rem;
+      background-color: white;
+      border-radius: 0.5rem;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    h2[_ngcontent-%COMP%] {
+      color: #904682; 
+
+    }
+
+    p[_ngcontent-%COMP%] {
+      line-height: 1.6;
+    }`]});let o=i;return o})();export{c as default};
---
+++ client/docs/client/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KOZ6UQKM.mjs";import"./chunk-H34COG2P.mjs";import"./chunk-KVZNOKX4.mjs";import"./chunk-QGOFUSSP.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
